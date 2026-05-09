6e6f4a25deb5f2d71049a1183680334d30b3a93c
AlexVplle <alexisvapaille11@gmail.com>
2026-05-09 13:50:10 +0200

    feat: add debug console extension (DBCN)

---
+++ src/debug_console.rs
@@ -0,0 +1,24 @@
+use super::{SbiRet, eid, sbi_call_1, sbi_call_3};
+
+const FID_WRITE: usize = 0;
+const FID_WRITE_BYTE: usize = 2;
+
+pub fn write(bytes: &[u8]) -> SbiRet {
+    unsafe {
+        sbi_call_3(
+            eid::DBCN,
+            FID_WRITE,
+            bytes.len(),
+            bytes.as_ptr() as usize,
+            0,
+        )
+    }
+}
+
+pub fn write_byte(byte: u8) -> SbiRet {
+    unsafe { sbi_call_1(eid::DBCN, FID_WRITE_BYTE, byte as usize) }
+}
+
+pub fn print(s: &str) -> SbiRet {
+    write(s.as_bytes())
+}
